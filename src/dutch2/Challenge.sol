d2339db159702da6d29263e175f189e6dcee353c
Harry Chen <a00012025@gmail.com>
2024-03-17 11:15:25 +0800

    feat: dutch2 problem

---
+++ src/dutch2/Challenge.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+import {AuctionManager} from "./AuctionManager.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
+
+contract Challenge {
+    AuctionManager public immutable auction;
+    ERC20 public immutable quoteToken;
+    ERC20 public immutable baseToken;
+
+    constructor(AuctionManager _auction, ERC20 _quoteToken, ERC20 _baseToken) {
+        auction = _auction;
+        quoteToken = _quoteToken;
+        baseToken = _baseToken;
+    }
+
+    function isSolved() external view returns (bool) {
+        // USDC balance = 0
+        return quoteToken.balanceOf(address(auction)) == 0;
+    }
+}
